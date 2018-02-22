2aedf992285b50568953017b254dc87bf65fed5a
Mark Karmar <mark.karmar@gmail.com>
2018-02-22 11:40:02 +0100

    working CBOW without predictor and simple ensamble working

--- Code/EnsamblePredictor.py
+++ Code/EnsamblePredictor.py
@@ -1,4 +1,6 @@
-import LearningAlgorithm
+import LeaningAlgoImpl.CBOW as cbow
+import StackingModelTrainer as l
+import os, logging
 """
 This module contains multiple simple ensamble methods
 """
@@ -11,29 +13,49 @@ This module contains multiple simple ensamble methods
     If multiple equaly good results are found, the firs to raise above the others in count of
     ocurences in predictions wins (if all predictors find different results, the first
     predictors result is the "best result")
+    This method returns the best result for a word, and the count for the word
 """
-def simple_majority_vote_ensamble(ListOfAlgorithms, ListOfPreTrainedModels):
-    Trainset = []
-    algorithms = []
-    models = []
-    results =[]
-    # Initialize the different
-    for names in ListOfAlgorithms:
-        algorithms.append(LearningAlgorithm(names, Trainset))
-    # Create the different submodels used for the ensamble learning
-    for alg in algorithms:
-        models.append(alg.train(Trainset))
-        print("Training new algorithm")
-    # Import previously created models, in order to save time by not
-    # training models already trained
-    for givenModel in ListOfPreTrainedModels:
-        models.append(givenModel)
-    for predictionModel in models:
-        results.append(predictionModel.predict())
-    # Predict using ensamble learning - simple majority vote
-    bestCount=0
-    for result in results:
-        if(bestCount < results.count(result)):
-            bestresult =result
-    return bestresult
+def simple_majority_vote_ensamble(params_lists):
+    models = [] # List of model-classes
+    result = [] # List of results from the different predictors
+    best_result = [None, 0] # Best result found
 
+    """
+        Setup of predictor classes
+    """
+    for i in params_lists[0]:
+        mod = cbow.CBOW()  # Initialize model
+        mod.get_model(hs =i[0], negative= i[1], cbow_mean=i[2], iter= i[3], size=i[4], min_count=i[5], max_vocab_size=i[6], workers=i[7]) #Train model
+        models.append(mod) #Add model class to list of trained model classes
+
+    """
+        Predict best word
+    """
+    for model in models:
+        result.append(model.predict()) #Predict from set and add to result list
+
+    """
+        Majority vote for best word
+    """
+    for res in result:
+        if(result.count(res)> best_result[1]): #Check if next result has a better "score"
+            best_result=[res, result.count(res)] #If better score, overwrite best result
+
+    return  best_result
+
+
+"""
+    A better ensamble learner, where the results for
+"""
+def result_combinatrion_ensamble(params_list):
+    print("not implemented yet")
+
+
+
+
+
+"""
+    The simple majority vote ensamble takes a 3 level array i.e. a tensor.
+    The outer layer for defining what type of word-embedding to run. 
+"""
+if __name__ == "__main__": simple_majority_vote_ensamble([[[1,5,0,10,100,5,None,3]]])
--- Code/LeaningAlgoImpl/CBOW.py
+++ Code/LeaningAlgoImpl/CBOW.py
@@ -4,28 +4,35 @@ from gensim.models import Word2Vec
 
 
 class CBOW:
+    model = None
 
-    def get_model(self):
+    def get_model(self, hs =1, negative= 5, cbow_mean=0, iter= 10, size=100, min_count=5, max_vocab_size=None, workers=3):
         dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         sentences1 = MySentences(dir_path + '/DataSet')      # Gets all files from folder at location.
         CBOW_model = Word2Vec(sentences=sentences1, #Sentences to train from
                               sg=1, #1 for CBOW, 0 for Skip-gram
-                              hs=1, #1 for hierarchical softmax and 0 and non-zero in negative argument then negative sampling is used.
-                              negative=5, #0 for no negative sampling and above specifies how many noise words should be drawn. (Usually 5-20 is good).
-                              cbow_mean=0, #0 for sum of context vectors, 1 for mean of context vectors. Only used on CBOW.
-                              iter=10, #number of epochs.
-                              size=100, #feature vector dimensionality
-                              min_count=5, #minimum frequency of words required
-                              max_vocab_size=None, #How much RAM is allowed, 10 million words needs approx 1GB RAM. None = infinite RAM
-                              workers=3, #How many threads are started for training.
+                              hs=hs, #1 for hierarchical softmax and 0 and non-zero in negative argument then negative sampling is used.
+                              negative=negative, #0 for no negative sampling and above specifies how many noise words should be drawn. (Usually 5-20 is good).
+                              cbow_mean=cbow_mean, #0 for sum of context vectors, 1 for mean of context vectors. Only used on CBOW.
+                              iter=iter, #number of epochs.
+                              size=size, #feature vector dimensionality
+                              min_count=min_count, #minimum frequency of words required
+                              max_vocab_size=max_vocab_size, #How much RAM is allowed, 10 million words needs approx 1GB RAM. None = infinite RAM
+                              workers=workers, #How many threads are started for training.
 
                               )
-        return CBOW_model
+        self.model = CBOW_model
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    def acc(self):
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        self.model.accuracy(dir_path + '/TestingSet/questions-words.txt')
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+    def predict(self):
+        print("I can't take this anymore")
 
-cbow = CBOW.get_model(CBOW)
+    def load_model(self, parth_to_model):
+        self.model = Word2Vec.load(parth_to_model)
 
-cbow.accuracy(dir_path + '/TestingSet/questions-words.txt')
+    def __init__(self):
+        self.model = None
