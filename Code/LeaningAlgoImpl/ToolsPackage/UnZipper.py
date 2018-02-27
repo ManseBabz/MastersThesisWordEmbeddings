3016a7bb04ad44c75ef4eb99c7e001cf19708860
Mark Karmar <mark.karmar@gmail.com>
2018-02-27 14:57:30 +0100

    Unzipper able to random sample articles with replacement

--- Code/LeaningAlgoImpl/ToolsPackage/UnZipper.py
+++ Code/LeaningAlgoImpl/ToolsPackage/UnZipper.py
@@ -1,9 +1,11 @@
 import zipfile, codecs
+import numpy.random as ran
 
 class ZippedSentences(object):
-    def __init__(self, zippedname, number_to_extract):
+    def __init__(self, zippedname, number_to_extract, random=False):
         self.zippedname = zippedname
         self.number_to_extract = number_to_extract
+        self.random = random
 
 
     def printinfo(self):
@@ -21,7 +23,11 @@ class ZippedSentences(object):
     def __iter__(self):
         i = 0
         with zipfile.ZipFile(self.zippedname) as myzip:
-            for file in myzip.filelist:
+            if(self.random==True):
+                list_of_files_to_use = ran.choice(myzip.filelist, size=self.number_to_extract, replace=True)
+            else:
+                list_of_files_to_use = myzip.filelist
+            for file in list_of_files_to_use:
                 if i <= self.number_to_extract:
                     i += 1
                     with myzip.open(file) as myfile:
