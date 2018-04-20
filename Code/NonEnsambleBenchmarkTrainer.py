a987dc3056ff3191925a0e8608452aae720445a5
Mark Karmar <mark.karmar@gmail.com>
2018-04-20 21:17:49 +0200

    Better text in nonensamblebenchmarktrainer

--- Code/NonEnsambleBenchmarkTrainer.py
+++ Code/NonEnsambleBenchmarkTrainer.py
@@ -32,7 +32,7 @@ def train_models():
         CBOW.get_model(hs=1, negative=10, cbow_mean=1, iter=10, size=1000, min_count=5,
                        max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         CBOW.save_model(model_name)
-        print(time.time()-start_time)
+        print("CBOW train-time " + str(time.time()-start_time))
 
     # train the SkipGram model
     Skip_gram = Skip.Skip_Gram(dev_mode=False)
@@ -44,7 +44,7 @@ def train_models():
         Skip_gram.get_model(hs=1, negative=10, cbow_mean=0, iter=10, size=1000, min_count=5,
                        max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         Skip_gram.save_model(model_name)
-        print(time.time() - start_time)
+        print("Skip-Gram train-time " + str(time.time() - start_time))
 
     Fast_Text = FT.Fast_Text(dev_mode=False)
     model_name = model_name_generator('Fast_Text', [1, 10, 1, 10, 1000, 5, 10000000], 1000000)
@@ -55,4 +55,4 @@ def train_models():
         Fast_Text.get_model(hs=1, negative=10, cbow_mean=1, iter=10, size=1000, min_count=5,
                        max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         Fast_Text.save_model(model_name)
-        print(time.time() - start_time)
+        print("FastText train-time " + str(time.time() - start_time))
