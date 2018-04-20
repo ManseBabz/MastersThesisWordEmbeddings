bf360f19b1e0c5d24a437a1ef2decddceb752bf9
Mark Karmar <mark.karmar@gmail.com>
2018-04-20 21:16:26 +0200

    Find train-time for nonensamble models

--- Code/NonEnsambleBenchmarkTrainer.py
+++ Code/NonEnsambleBenchmarkTrainer.py
@@ -1,6 +1,7 @@
 import LeaningAlgoImpl.CBOW as CB
 import LeaningAlgoImpl.Skip_Gram as Skip
 import LeaningAlgoImpl.Fast_Text as FT
+import time
 import os
 
 def model_name_generator(model_type, model_params, training_articles):
@@ -20,16 +21,18 @@ def model_exists_checker(model_name, dev_mode):
         return True
     else:
         return False
-    
+
 def train_models():
     CBOW = CB.CBOW(dev_mode=False)
     model_name = model_name_generator('CBOW', [1, 10, 1, 10, 1000, 5, 10000000], 1000000)
     if model_exists_checker(model_name, dev_mode=False):
         print("Model already exists")
     else:
+        start_time = time.time()
         CBOW.get_model(hs=1, negative=10, cbow_mean=1, iter=10, size=1000, min_count=5,
-                       max_vocab_size=10000000, workers=6, articles_to_learn=1000000, randomTrain=True)
+                       max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         CBOW.save_model(model_name)
+        print(time.time()-start_time)
 
     # train the SkipGram model
     Skip_gram = Skip.Skip_Gram(dev_mode=False)
@@ -37,15 +40,19 @@ def train_models():
     if model_exists_checker(model_name, dev_mode=False):
         print("Model already exists")
     else:
+        start_time = time.time()
         Skip_gram.get_model(hs=1, negative=10, cbow_mean=0, iter=10, size=1000, min_count=5,
-                       max_vocab_size=10000000, workers=6, articles_to_learn=1000000, randomTrain=True)
+                       max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         Skip_gram.save_model(model_name)
+        print(time.time() - start_time)
 
     Fast_Text = FT.Fast_Text(dev_mode=False)
     model_name = model_name_generator('Fast_Text', [1, 10, 1, 10, 1000, 5, 10000000], 1000000)
     if model_exists_checker(model_name, dev_mode=False):
         print("Model already exists")
     else:
+        start_time = time.time()
         Fast_Text.get_model(hs=1, negative=10, cbow_mean=1, iter=10, size=1000, min_count=5,
-                       max_vocab_size=10000000, workers=6, articles_to_learn=1000000, randomTrain=True)
+                       max_vocab_size=10000000, workers=3, articles_to_learn=1000000, randomTrain=True)
         Fast_Text.save_model(model_name)
+        print(time.time() - start_time)
