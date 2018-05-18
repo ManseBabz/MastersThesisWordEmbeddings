80bb69886a056d5fc67c43d1552b7ef8ccbdb0eb
ManseBabz <dansemanse_den_1@hotmail.com>
2018-05-18 11:48:58 +0200

    added position based majority vote

--- Code/own_enseemble.py
+++ Code/own_enseemble.py
@@ -8,14 +8,14 @@ from collections import Counter
 class own_enseemble:
 
     def simple_majority_vote(self, questions, topn = 1):
-        guesses = self.get_multiple_results(questions, number_of_models=9, topn=topn)
+        guesses = self.get_multiple_results(questions, number_of_models=15, topn=topn)
         reals = self.get_expected_acc_results(questions)
 
         combined_guesses = []
         for i in range(len(reals)):
             combined_guess = []
             for guess in guesses:
-                print(guess[i])
+                #print(guess[i])
 
                 try:
                     for g in guess[i]:
@@ -33,17 +33,17 @@ class own_enseemble:
         for guess in combined_guesses:
             count = Counter(guess)
             most_common = count.most_common(1)[0]
-            print(most_common)
+            #print(most_common)
             if(most_common[0] is None):
                 #print('hej')
                 try:
                     most_common = count.most_common(2)[1]
                 except IndexError:
-                    print('No model has an answer')
+                    #print('No model has an answer')
                     pass
 
             final_guess.append(most_common[0])
-        print(final_guess)
+        #print(final_guess)
 
         correct = []
         wrong = []
@@ -66,6 +66,100 @@ class own_enseemble:
         print('Wrong ' + str(number_of_wrong))
         print(wrong)
 
+    def position_based_tie_handling_majority_vote(self, questions, topn = 1):
+        guesses = self.get_multiple_results(questions, number_of_models=15, topn=topn)
+        reals = self.get_expected_acc_results(questions)
+
+        combined_guesses = []
+        for i in range(len(reals)):
+            combined_guess = []
+            for guess in guesses:
+                #print(guess[i])
+
+                try:
+                    for j in range(0, len(guess[i])):
+                        combined_guess.append((guess[i][j], j))
+                except TypeError:
+                    combined_guess.append(guess[i])
+
+                #combined_guess.append(guess[i])
+            combined_guesses.append(combined_guess)
+
+        #print(combined_guesses)
+        #print(reals)
+
+        final_guess = []
+        for guess in combined_guesses:
+            count = self.position_counter(guess)
+            most_common = count[0]
+            #print(most_common)
+            if(most_common[0] is None):
+                #print('hej')
+                try:
+                    most_common = count[1]
+                except IndexError:
+                    #print('No model has an answer')
+                    pass
+            #print(most_common[0])
+            final_guess.append(most_common[0])
+        #print(final_guess)
+
+        correct = []
+        wrong = []
+        number_of_correct = 0
+        number_of_wrong = 0
+        for i in range(0, len(final_guess)):
+            predicted = final_guess[i]
+            expected = reals[i]
+            if predicted == expected:
+                correct_message = ("predicted: %s correct" % (predicted))
+                correct.append(correct_message)
+                number_of_correct += 1
+            else:
+                wrong_message = ("predicted: %s, should have been: %s" % (predicted, expected))
+                wrong.append(wrong_message)
+                number_of_wrong += 1
+
+        print('Correct ' + str(number_of_correct))
+        print(correct)
+        print('Wrong ' + str(number_of_wrong))
+        print(wrong)
+
+    def position_counter(self, guess_list):
+        # print('guess')
+        #print(guess_list)
+        combined_guess_list = []
+        # print(guess_list[0])
+
+        # if guess_list is None:
+        # return [[None]]
+        for i in range(0, len(guess_list)):
+            if guess_list[i] is not None:
+                guess, value = guess_list[i]
+                not_found = True
+                for i in range(0, len(combined_guess_list)):
+                    word, old_value, number_of_appearances = combined_guess_list[i]
+                    if guess == word:
+                        combined_guess_list[i] = [guess, value + old_value, number_of_appearances + 1]
+                        not_found = False
+
+                if not_found:
+                    combined_guess_list.append((guess, value, 1))
+            else:
+                # print('None')
+                combined_guess_list.append((None, 0, 0))
+                # print(combined_guess_list)
+
+        sorted_combined_guess_list = sorted(combined_guess_list, key=lambda x: x[2], reverse=True)
+        #print(sorted_combined_guess_list)
+        max_number_of_appearance = []
+        for i in range(0, len(sorted_combined_guess_list)):
+            #print(sorted_combined_guess_list[i])
+            if sorted_combined_guess_list[0][2] == sorted_combined_guess_list[i][2]:
+                max_number_of_appearance.append((sorted_combined_guess_list[i][0], sorted_combined_guess_list[i][1]))
+        sorted_combined_guess_list = sorted(max_number_of_appearance, key=lambda x: x[1], reverse=False)
+        #print(sorted_combined_guess_list)
+        return sorted_combined_guess_list
 
     def get_multiple_results(self, questions, number_of_models, topn):
         name_array = []
@@ -78,12 +172,12 @@ class own_enseemble:
             else:
                 name_array.append(onlyfiles[file_index])
 
-        print(name_array)
+        #print(name_array)
         models = []
         i = 0
         for name in name_array:
             if number_of_models > i:
-                print(name)
+                #print(name)
                 finished_model = FM.Finished_Models()
                 finished_model.get_model(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models/" + name)
                 models.append(finished_model)
@@ -91,17 +185,17 @@ class own_enseemble:
             else:
                 continue
 
-        print(models)
+        #print(models)
 
         results = []
         for model in models:
             results.append(model.get_acc_results(topn, questions))
 
-        print(results)
+        #print(results)
         return results
 
-    def tie_handling_majority_vote(self, questions, topn = 1):
-        guesses = self.get_multiple_tie_handling_results(questions, number_of_models=9, topn=topn)
+    def certainess_tie_handling_majority_vote(self, questions, topn = 1):
+        guesses = self.get_multiple_certainess_tie_handling_results(questions, number_of_models=15, topn=topn)
         reals = self.get_expected_acc_results(questions)
 
         combined_guesses = []
@@ -192,7 +286,7 @@ class own_enseemble:
 
 
 
-    def get_multiple_tie_handling_results(self, questions, number_of_models, topn):
+    def get_multiple_certainess_tie_handling_results(self, questions, number_of_models, topn):
         name_array = []
         not_wanted = 'npy'
         onlyfiles = [f for f in listdir(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models") if
@@ -203,12 +297,12 @@ class own_enseemble:
             else:
                 name_array.append(onlyfiles[file_index])
 
-        print(name_array)
+        #print(name_array)
         models = []
         i = 0
         for name in name_array:
             if number_of_models > i:
-                print(name)
+                #print(name)
                 finished_model = FM.Finished_Models()
                 finished_model.get_model(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models/" + name)
                 models.append(finished_model)
@@ -216,7 +310,7 @@ class own_enseemble:
             else:
                 continue
 
-        print(models)
+        #print(models)
 
         results = []
         for model in models:
@@ -270,4 +364,6 @@ enseemble_test = own_enseemble()
 
 enseemble_test.simple_majority_vote('danish-topology.txt', topn=10)
 
-enseemble_test.tie_handling_majority_vote('danish-topology.txt', topn=10)
+enseemble_test.certainess_tie_handling_majority_vote('danish-topology.txt', topn=10)
+
+enseemble_test.position_based_tie_handling_majority_vote('danish-topology.txt', topn=10)
