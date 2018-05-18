02c560fdaad5be3b89be90ec683dcbac7a26cf17
Mark Karmar <mark.karmar@gmail.com>
2018-05-18 09:47:05 +0200

    preliminary results and their method

--- Code/EnsamblePredictor.py
+++ Code/EnsamblePredictor.py
@@ -530,6 +530,147 @@ class boot_strap_aggregator:
         #print(results)
         return results
 
+
+
+
+
+
+
+
+    def tie_handling_majority_vote(self, questions, topn = 1, number_of_models=10):
+        guesses = self.get_multiple_tie_handling_results(questions, number_of_models=number_of_models, topn=topn)
+        reals = self.get_expected_acc_results(questions)
+
+        combined_guesses = []
+        for i in range(len(reals)):
+            combined_guess = []
+            for guess in guesses:
+                #print(guess[i])
+
+                try:
+                    for g in guess[i]:
+                        combined_guess.append(g)
+                except TypeError:
+                    combined_guess.append(guess[i])
+
+                #combined_guess.append(guess[i])
+            combined_guesses.append(combined_guess)
+
+        #print(combined_guesses)
+        #print(reals)
+
+        final_guess = []
+        for guess in combined_guesses:
+            count = self.weighted_counter(guess)
+            most_common = count[0]
+            #print(most_common)
+            if(most_common[0] is None):
+                #print('hej')
+                try:
+                    most_common = count[1]
+                except IndexError:
+                    #print('No model has an answer')
+                    pass
+            #print(most_common[0])
+            final_guess.append(most_common[0])
+        #print(final_guess)
+
+        correct = []
+        wrong = []
+        number_of_correct = 0
+        number_of_wrong = 0
+        for i in range(0, len(final_guess)):
+            predicted = final_guess[i]
+            expected = reals[i]
+            if predicted == expected:
+                correct_message = ("predicted: %s correct" % (predicted))
+                correct.append(correct_message)
+                number_of_correct += 1
+            else:
+                wrong_message = ("predicted: %s, should have been: %s" % (predicted, expected))
+                wrong.append(wrong_message)
+                number_of_wrong += 1
+
+        print('Correct ' + str(number_of_correct))
+        #print(correct)
+        print('Wrong ' + str(number_of_wrong))
+        #print(wrong)
+        return number_of_correct, number_of_wrong
+
+    def weighted_counter(self, guess_list):
+        #print('guess')
+        #print(guess_list)
+        combined_guess_list = []
+        #print(guess_list[0])
+
+        #if guess_list is None:
+            #return [[None]]
+        for i in range(0, len(guess_list)):
+            if guess_list[i] is not None:
+                guess, value = guess_list[i]
+                not_found = True
+                for i in range(0, len(combined_guess_list)):
+                    word, old_value = combined_guess_list[i]
+                    if guess == word:
+                        combined_guess_list[i] = (guess, value + old_value)
+                        not_found = False
+
+                if not_found:
+                    combined_guess_list.append((guess, value))
+            else:
+                #print('None')
+                combined_guess_list.append((None, 0))
+            #print(combined_guess_list)
+
+        sorted_combined_guess_list = sorted(combined_guess_list, key=lambda x: x[1], reverse=True)
+        #print(sorted_combined_guess_list)
+        return sorted_combined_guess_list
+
+
+
+
+
+    def get_multiple_tie_handling_results(self, questions, number_of_models, topn):
+        name_array = []
+        not_wanted = ['npy', 'Readme.md']
+        onlyfiles = [f for f in listdir(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models") if
+                     isfile(join(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models", f))]
+        for file_index in range(0, len(onlyfiles)):
+            if (not_wanted[0] in onlyfiles[file_index] or not_wanted[1]in onlyfiles[file_index]):
+                continue
+            else:
+                name_array.append(onlyfiles[file_index])
+
+        #print(name_array)
+        models = []
+        random.shuffle(name_array)
+        i = 0
+        for name in name_array:
+            if number_of_models > i:
+                print(name)
+                finished_model = FM.Finished_Models()
+                finished_model.get_model(os.path.dirname(os.path.realpath(__file__)) + "/LeaningAlgoImpl/Models/" + name)
+                models.append(finished_model)
+                i += 1
+            else:
+                continue
+
+        #print(models)
+
+        results = []
+        for model in models:
+            results.append(model.get_acc_results_extra(topn, questions))
+
+        #print(results)
+        return results
+
+
+
+
+
+
+
+
     def get_expected_acc_results(self, questions):
         dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         questions = dir_path + '/Code/TestingSet/' + questions
@@ -622,8 +763,13 @@ class boot_strap_aggregator:
             #sections.append(total)
             return sections
         else:
-            correct, wrong = boot_strap_aggregator.majority_vote_fast(self, questions=questions, topn=10, number_of_models=number_of_models)
-            return correct, wrong
+            if(predictor_method== 0):
+                correct, wrong = boot_strap_aggregator.majority_vote_fast(self, questions=questions, topn=10, number_of_models=number_of_models)
+                return correct, wrong
+            elif(predictor_method==3):
+                correct, wrong = boot_strap_aggregator.tie_handling_majority_vote(self, questions=questions, topn=10,
+                                                                          number_of_models=number_of_models)
+                return correct, wrong
 
     def evaluate_word_pairs(self, pairs, delimiter='\t', restrict_vocab=300000,
                             case_insensitive=False, dummy4unknown=False, similarity_model_type="0"):
--- Code/Statistical_Simulator.py
+++ Code/Statistical_Simulator.py
@@ -6,12 +6,13 @@ import keyboard
 
 def generate_statistics(startingpoint = 5, endpoint = 50, skips = 5, iterations = 5, topn=10):
     results = []
-    if(os.path.isfile("Ensamble_test_results.csv") ):
-        f = open("Ensamble_test_results.csv", "a")
-    else:
-        f = open("Ensamble_test_results.csv", "w")
+
     for i in range(startingpoint, endpoint, skips):
         for j in range(0, iterations):
+            if (os.path.isfile("Ensamble_test_results.csv")):
+                f = open("Ensamble_test_results.csv", "a")
+            else:
+                f = open("Ensamble_test_results.csv", "w")
             print(j)
             ensamble_model = EP.boot_strap_aggregator()
             dir_path = "questions-words.txt"
@@ -19,17 +20,16 @@ def generate_statistics(startingpoint = 5, endpoint = 50, skips = 5, iterations 
             res = [[i, topn, right, wrong]]
             print(res)
             results.append(res)
-            if (os.path.isfile("Ensamble_test_results.csv")):
-                f = open("Ensamble_test_results.csv", "a")
-            else:
-                f = open("Ensamble_test_results.csv", "w")
             np.savetxt(f, res, delimiter=',')
             f.close()
+            print('iteration finished')
+
+
     print(results)
 
     return results
 
-def experiment(startingpoint=5, endpoint=400, skips=50, iterations=5, topn=10):
+def experiment(startingpoint=5, endpoint=30, skips=5, iterations=5, topn=10):
     while True:
         try:  # used try so that if user pressed other than the given key error will not be shown
             if keyboard.is_pressed(' '):  # if key 'q' is pressed
@@ -37,12 +37,45 @@ def experiment(startingpoint=5, endpoint=400, skips=50, iterations=5, topn=10):
                 print('finishing')
                 break  # finishing the loop
             else:
-                generate_statistics(startingpoint=random.randint(1, startingpoint),
-                                    endpoint=random.randint(startingpoint + 1, endpoint),
+                start =random.randint(startingpoint, endpoint)
+                generate_statistics(startingpoint=start,
+                                    endpoint=random.randint(start + 1, endpoint),
                                     skips=random.randint(1, skips), iterations=random.randint(1, iterations),
                                     topn=random.randint(1, topn))
         except:
             break
+
+def generate_statistics_with_weighted_majorityvote_ensamble(startingpoint = 5, endpoint = 50, skips = 5, iterations = 5, topn=10):
+    results = []
+
+    for i in range(startingpoint, endpoint, skips):
+        for j in range(0, iterations):
+            if (os.path.isfile("weighted_majority_vote_Ensamble_test_results.csv")):
+                f = open("weighted_majority_vote_Ensamble_test_results.csv", "a")
+            else:
+                f = open("weighted_majority_vote_Ensamble_test_results.csv", "w")
+            print(j)
+            ensamble_model = EP.boot_strap_aggregator()
+            dir_path = "questions-words.txt"
+            right, wrong = ensamble_model.accuracy(dir_path, number_of_models=i, predictor_method=3)
+            res = [[i, topn, right, wrong]]
+            print(res)
+            results.append(res)
+            np.savetxt(f, res, delimiter=',')
+            f.close()
+            print('iteration finished')
+
+    print(results)
+
+    return results
+def experiment2(startingpoint=20, endpoint=200, skips=5, iterations=2, topn=10):
+
+            start =random.randint(startingpoint, endpoint)
+            generate_statistics_with_weighted_majorityvote_ensamble(startingpoint=startingpoint,
+                                endpoint=random.randint(start + 1, endpoint),
+                                skips=random.randint(1, skips), iterations=random.randint(1, iterations),
+                                topn=random.randint(1, topn))
+
 
 
 
